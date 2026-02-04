82501e9c07e25cb9894051247670570aedce4b5b
Russell Foo <iruss1337@gmail.com>
2026-02-05 06:28:05 +0800

    feat: free old stash in ft_strjoin to prevent leak

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: russ1337 <russ1337@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 22:10:48 by rfoo              #+#    #+#             */
-/*   Updated: 2026/02/05 03:59:14 by russ1337         ###   ########.fr       */
+/*   Updated: 2026/02/05 04:34:45 by russ1337         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*get_next_line(int fd);
 
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: russ1337 <russ1337@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 22:10:43 by rfoo              #+#    #+#             */
-/*   Updated: 2026/02/05 03:58:07 by russ1337         ###   ########.fr       */
+/*   Updated: 2026/02/05 06:26:48 by russ1337         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,12 +54,36 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (dest);
 }
 
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	dest = malloc(ft_strlen(s) + 1);
+	if (!dest)
+		return (NULL);
+	while (s[i])
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	strlen1;
 	size_t	strlen2;
 	char	*str;
 
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
 	strlen1 = ft_strlen(s1);
 	strlen2 = ft_strlen(s2);
 	str = malloc(strlen1 + strlen2 + 1);
@@ -68,5 +92,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ft_memcpy(str, s1, strlen1);
 	ft_memcpy(str + strlen1, s2, strlen2);
 	str[strlen1 + strlen2] = '\0';
+	free(s1);
 	return (str);
 }
