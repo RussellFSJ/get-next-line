8d0bb59999f86555597ccae811afbd821a4cd276
Russell Foo <iruss1337@gmail.com>
2026-02-05 04:00:54 +0800

    chore: update header file with all non-static functions

--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rfoo <rfoo@student.42singapore.sg>         +#+  +:+       +#+        */
+/*   By: russ1337 <russ1337@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 22:10:48 by rfoo              #+#    #+#             */
-/*   Updated: 2026/01/28 22:31:03 by rfoo             ###   ########.fr       */
+/*   Updated: 2026/02/05 03:59:14 by russ1337         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,10 @@
 
 # define BUFFER_SIZE 1024
 
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-char	*ft_strchr(const char *s, int c);
-
+char	*ft_strjoin(char const *s1, char const *s2);
 char	*get_next_line(int fd);
-char	*get_line(const char *s);
 
 #endif
